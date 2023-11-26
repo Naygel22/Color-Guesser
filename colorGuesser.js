18f52d2a4c343b344693d4df1cbe74af74f16028
Naygel22 <piotrkozlowski14@gmail.com>
2023-11-26 17:36:07 +0100

    color change after guess

--- colorGuesser.js
+++ colorGuesser.js
@@ -5,12 +5,11 @@ const boxes = document.querySelectorAll('.box');
 const colorName = document.querySelector('.colorName');
 
 
-
-
 for(let i = 0; i < 3; i++){
   createSquare(boxesFirstRow);
   createSquare(boxesSecondRow);
 }
+
 
 newColorsButton.addEventListener('click', () => {
  const currentBoxes = document.querySelectorAll('.box')
@@ -25,12 +24,16 @@ newColorsButton.addEventListener('click', () => {
   currentBoxes.forEach(box => {
     box.addEventListener('click', () => {
         if (colorName.textContent === box.style.backgroundColor) {
-             return alert('Dobrze!');
+          currentBoxes.forEach(box =>{
+            box.style.backgroundColor = generateRandomColor();
+          })
         }
     });
 });
   
 });
+
+
 
 function createSquare (row) {
   const box = document.createElement('div');
@@ -40,13 +43,6 @@ function createSquare (row) {
 
   return box;
 }
-
-  
-
-
-
-
-
 
 function generateRandomColor(){
   let maxVal = 0xFFFFFF; // 16777215
